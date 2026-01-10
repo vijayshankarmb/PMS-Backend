61bfd5abc19f19deb638793144ddfc5e8478bc50
Vijay Shankar <vijayshankarmb@gmail.com>
2026-01-10 16:04:00 +0530

    fix

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -67,8 +67,8 @@ export const login = async (req: Request, res: Response) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      secure: true,
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000
     });
 
@@ -91,8 +91,8 @@ export const logout = async (req: Request, res: Response) => {
   try {
     res.cookie("token", "", {
       httpOnly: true,
-      secure: true,
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 0
     });
     return res.status(200).json({
